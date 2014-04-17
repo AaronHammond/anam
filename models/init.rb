0aaf4e935ae7d4e47abb5006612a4a09eca0adca
AaronHammond <ahammond@mit.edu>
2014-04-17 17:25:56 -0400

    now we can actually test locally! it's almost like we're real software engineers

--- models/init.rb
+++ models/init.rb
@@ -1,28 +1,32 @@
 # encoding: utf-8
+
 require 'sequel'
-DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-DB << "SET CLIENT_ENCODING TO 'UTF8';"
 
-# DB = Sequel.sqlite
+if ENV['ANAM_TEST'] then
+	DB = Sequel.sqlite
 
+	DB.create_table :users do
+		primary_key :id
+		varchar :username, :index=>true
+		varchar :email
+		varchar :access_token
+	end
 
-#DB.create_table :users do
-#	primary_key :id
-#	varchar :username, :index=>true
-#	varchar :email
-#	varchar :access_token
-#end
+	DB.create_table :backups do
+		primary_key :id
+		timestamp :last_backup
+		varchar :repo_target
+		varchar :s3_link
+		boolean :active, :default=>true
+		varchar :external_id, :index=>true
+		varchar :webhook_id
+		foreign_key :user_id, :users
+	end
+else 
+	DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+	DB << "SET CLIENT_ENCODING TO 'UTF8';"
+end
 
-#DB.create_table :backups do 
-#	primary_key :id
-#	timestamp :last_backup
-#	varchar :repo_target
-#	varchar :s3_link
-#	boolean :active, :default=>true
-#	varchar :external_id, :index=>true
-#	varchar :webhook_id
-#	foreign_key :user_id, :users
-#end
 
 require_relative 'backup'
 require_relative 'user'
